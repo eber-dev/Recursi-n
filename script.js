80f664b44a38e6d1aead3dd2a529497d4d22e52c
Eber <www.eberacunaquispe@gmail.com>
2026-04-20 15:38:49 -0500

    Se añadio la funcion de merge sort

--- script.js
+++ script.js
@@ -27,4 +27,32 @@ function fibsrec(num) {
 console.log(fibsrec(5));
 console.log(fibs(5));
 
-function mergeSort() {}
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array;
+    }
+
+    const mitad = Math.floor(array.length / 2);
+    const primeramitad = array.slice(0, mitad);
+    const segundamitad = array.slice(mitad);
+
+    return merge(primeramitad, segundamitad);
+}
+
+function merge(a, b) {
+    let resultado = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < a.length && j < b.length) {
+        if (a[i] < b[j]) {
+            resultado.push(a[i]);
+            i++;
+        } else {
+            resultado.push(b[j]);
+            j++;
+        }
+    }
+
+    return resultado.concat(a.slice(i)).concat(b.slice(j));
+}
